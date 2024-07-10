f8533c11b6c28cdd6010dc87a931425bf56f81f7
joselitopapi14 <enriquejo2002@gmail.com>
2024-07-10 11:06:32 -0500

    Comienzo del desafio

---
+++ src/main/java/com/example/LiteraAlura/LiteraAluraApplication.java
@@ -0,0 +1,13 @@
+package com.example.LiteraAlura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraAluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraAluraApplication.class, args);
+	}
+
+}
